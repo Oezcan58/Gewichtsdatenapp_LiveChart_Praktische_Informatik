18dfab2bc9fd9fb467341cf49eaf275a0a4f7a67
Oezcan58 <102741258+Oezcan58@users.noreply.github.com>
2025-01-15 08:28:57 +0100

    Update WerteSeite.xaml.cs

--- Gewichtsdatenapp_LiveChart/View/WerteSeite.xaml.cs
+++ Gewichtsdatenapp_LiveChart/View/WerteSeite.xaml.cs
@@ -1,22 +1,14 @@
-using System.Collections.ObjectModel;
-using Gewichtsdatenapp_LiveChart.Model;
 using Gewichtsdatenapp_LiveChart.ViewModel;
 
 namespace Gewichtsdatenapp_LiveChart.View;
 
 public partial class WerteSeite : ContentPage
 {
-    public WerteSeite()
+    public WerteSeite(BaseViewModel baseViewModel)
     {
+        BindingContext = baseViewModel;
         InitializeComponent();
     }
 
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-       if (BindingContext is BaseViewModel viewModel) // Daten neu laden
-        {
-            await viewModel.ReloadDataAsync();
-        }
-    }
+
 }
