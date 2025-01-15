5cba2a347770c6dedb438465a550ae4a95be9059
Oezcan58 <102741258+Oezcan58@users.noreply.github.com>
2025-01-15 08:31:44 +0100

    Create WeightClass.cs

---
+++ Gewichtsdatenapp_LiveChart/Enums/WeightClass.cs
@@ -0,0 +1,11 @@
+namespace Gewichtsdatenapp_LiveChart.Enums
+{
+    public enum WeightClass// Enum zur Kategorisierung des Gewichts basierend auf dem BMI
+    {
+        Untergewicht,
+        Normalgewicht,
+        Übergewicht,
+        Adipositas,
+        NichtGeeignet,
+    }
+}
