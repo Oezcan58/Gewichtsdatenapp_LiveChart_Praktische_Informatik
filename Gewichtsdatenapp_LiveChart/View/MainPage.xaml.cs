5da7a23ff9d85acb69044a421123358107a995e2
Oezcan58 <102741258+Oezcan58@users.noreply.github.com>
2025-01-15 08:28:05 +0100

    Update MainPage.xaml.cs

--- Gewichtsdatenapp_LiveChart/View/MainPage.xaml.cs
+++ Gewichtsdatenapp_LiveChart/View/MainPage.xaml.cs
@@ -1,21 +1,16 @@
-﻿using Gewichtsdatenapp_LiveChart.ViewModel;
+using Gewichtsdatenapp_LiveChart.ViewModel;
 
 namespace Gewichtsdatenapp_LiveChart.View
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        public MainPage(BaseViewModel baseViewModel)
         {
+            BindingContext = baseViewModel;
             InitializeComponent();
         }
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-            if (BindingContext is BaseViewModel viewModel) // Daten neu laden
-            {
-                await viewModel.ReloadDataAsync();
-            }
-        }
+
     }
 }
+
 
